5817e9e7ab09dd853ae53dd7064865ed44631188
ferror <zmalcherczyk@gmail.com>
2020-12-28 02:47:55 +0100

    vue router

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import store  from '@/store'
+import router from './router'
 
 import App from './App.vue'
 
+
 const app = createApp(App);
 
+app.use(router);
 app.use(store);
 app.mount('#app');
