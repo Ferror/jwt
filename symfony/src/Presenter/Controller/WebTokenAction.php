f8dd2d38fce04f21f29e238dac78691ef983f9d5
ferror <zmalcherczyk@gmail.com>
2021-04-25 17:34:17 +0200

    remove commented code

--- symfony/src/Presenter/Controller/WebTokenAction.php
+++ symfony/src/Presenter/Controller/WebTokenAction.php
@@ -4,15 +4,9 @@ declare(strict_types=1);
 namespace App\Presenter\Controller;
 
 use App\Application\WebTokenEncoder;
-use App\Domain\Clock;
 use App\Domain\Credentials;
-use App\Domain\User\UserIdentifier;
 use App\Domain\User\UserStorage;
-use App\Domain\WebToken;
-use App\Domain\WebToken\Algorithm;
-use App\Domain\WebToken\WebTokenHeader;
 use App\Domain\WebTokenFactory;
-use App\Domain\WebTokenPayload;
 use App\Framework\Response\ErrorResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -44,37 +38,7 @@ final class WebTokenAction extends AbstractController
         $user = $this->userStorage->get($credentials->getLogin()->toString());
 
         if ($user->isPasswordValid($credentials->getPassword())) {
-//            $header = ['alg' => 'SHA512'];
-//            $payload = [
-//                'user' => [
-//                    'identifier' => 123,
-//                ],
-//                'created_at' => $this->clock->getTime(),
-//                'expires_at' => $this->clock->getTime(),
-//            ];
-//            $signature = \hash_hmac($header['alg'], json_encode($header) . json_encode($payload), 'secret');
-//
-//            return new JsonResponse(
-//                [
-//                    'token' => $this->encoder->encode($header) . '.' . $this->encoder->encode($payload) . '.' . $signature,
-//                ],
-//                200
-//            );
-
-//            $header = new WebTokenHeader(Algorithm::sha512());
-//            $payload = new WebTokenPayload($this->clock->getTime(), $this->clock->getTime(), $user->getIdentifier());
-//            $token = new WebToken(
-//                $header,
-//                $payload,
-//                new WebToken\WebTokenSignature(
-//                    \hash_hmac(
-//                        Algorithm::sha512()->toString(),
-//                        json_encode($header->jsonSerialize()) . json_encode($payload->jsonSerialize()),
-//                        'secret'
-//                    )
-//                )
-//            );
-
+            //Save token to Redis & Database
             $token = $this->factory->create($user);
 
             return new JsonResponse(['token' => $token->serialize($this->encoder)], 200);
